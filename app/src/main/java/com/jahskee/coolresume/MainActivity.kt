cd08a8f2cd82c85645652fe0352464e086a91155
jahskee <Wingman2i>
2019-02-09 02:54:00 -0500

    add click workHistoryBtn
    Signed-off-by: jahskee <Wingman2i>

--- app/src/main/java/com/jahskee/coolresume/MainActivity.kt
+++ app/src/main/java/com/jahskee/coolresume/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.jahskee.coolresume
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // add click event to workHistoryBtn
+        var workHistoryBtn = findViewById<Button>(R.id.workHistoryBtn)
+        workHistoryBtn.setOnClickListener {
+            println("Hey you hit the button")
+
+            var moveToWorkHistory = Intent(getApplicationContext(), WorkHistorActivity::class.java)
+            startActivity(moveToWorkHistory)
+        }
     }
 }
