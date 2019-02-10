3bfe05a3fc417866be5a3a4f7dc46fbeb5d189fc
jahskee <Wingman2i>
2019-02-10 02:54:48 -0500

    update
    Signed-off-by: jahskee <Wingman2i>

--- app/src/main/java/com/jahskee/coolresume/MainActivity.kt
+++ app/src/main/java/com/jahskee/coolresume/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.jahskee.coolresume
 
 import android.content.Intent
+import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -12,12 +13,38 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // add click event to workHistoryBtn
-        var workHistoryBtn = findViewById<Button>(R.id.workHistoryBtn)
-        workHistoryBtn.setOnClickListener {
+        var workHistoryButton = findViewById<Button>(R.id.workHistoryButton)
+        workHistoryButton.setOnClickListener {
             println("Hey you hit the button")
 
             var moveToWorkHistory = Intent(getApplicationContext(), WorkHistorActivity::class.java)
             startActivity(moveToWorkHistory)
         }
+
+        // add click event to call
+        var callButton = findViewById<Button>(R.id.callButton)
+        callButton.setOnClickListener {
+            println("Hey you hit the call button")
+
+            var phoneUri = Uri.parse("tel: 5712979957")
+            var callIntent = Intent(Intent.ACTION_DIAL, phoneUri)
+            startActivity(callIntent)
+        }
+
+
+        // add click event to call
+        var emailButton = findViewById<Button>(R.id.emailButton)
+        emailButton.setOnClickListener {
+            println("Hey you hit the email button")
+
+            var emailIntent = Intent(Intent.ACTION_SEND)
+            emailIntent.setType("plain/text")
+            emailIntent.putExtra(Intent.EXTRA_EMAIL, "jaizon.lubaton@yahoo.com")
+            emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Nice Resume")
+            emailIntent.putExtra(Intent.EXTRA_TEXT, "I Really enjoyed your resume..." )
+            startActivity(emailIntent)
+        }
+
+
     }
 }
